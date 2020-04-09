08fdd695b11dd97e45f7571c31ee1d9aa1e92ae8
Vlad <vladtuluman@gmail.com>
2020-04-09 15:13:16 +0300

    Done

---
+++ Client/SomeData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client
+{
+    class SomeData
+    {
+        public string dataId { get; set; }
+        public int weight { get; set; }
+    }
+}
